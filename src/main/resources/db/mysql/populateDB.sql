b6540d8cb2cb254dcb0bbbd7b4c44287db8fc263
Administrator <Administrator@172.31.16.232>
2017-10-28 02:34:00 +0000

    changes in db and jsp

--- src/main/resources/db/mysql/populateDB.sql
+++ src/main/resources/db/mysql/populateDB.sql
@@ -51,3 +51,5 @@ INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'RALLoans');
 INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'debtColl');
 INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'paydayLoans');
 INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'insure');
+
+
